284584078c095ca2de28bbe05a7f7995d7868440
Luca Valsecchi <luca.valsecchi@noku.io>
2023-04-29 22:49:49 +0200

    feat(lib): update biosspy

--- heartbeat.py
+++ heartbeat.py
@@ -1,4 +1,3 @@
-from ecgdetectors import Detectors
 from flask import Flask, request, jsonify
 from biosppy.signals import ecg
 
@@ -8,17 +7,16 @@ app = Flask(__name__)
 @app.route('/rr', methods=['POST'])
 def rr():
     frequency = 125
-    detectors = Detectors(frequency)
 
     data = request.get_json()
     # pass ecg parameter to the function and get the r peaks
     # ecg model = [-200, -100, -56, 3, 200, ...]
     my_ecg = data['ecg']
-    rr_peaks = detectors.engzee_detector(my_ecg)
-    int_rr_array = [int(item) for item in rr_peaks]
 
     out = ecg.ecg(signal=my_ecg, sampling_rate=125, show=False)
     bpm = out["heart_rate"]
+    rr_peaks = out["rpeaks"]
+    int_rr_array = [int(item) for item in rr_peaks]
     int_bpm_array = [int(item) for item in bpm]
 
     return jsonify({'rr': int_rr_array, 'bpm': int_bpm_array})
