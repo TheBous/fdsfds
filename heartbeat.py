0e642a31c8b6534c90c8ff86d6f5d98ad51a2094
Luca Valsecchi <luca.valsecchi@noku.io>
2023-04-29 23:24:44 +0200

    chore(comments)

--- heartbeat.py
+++ heartbeat.py
@@ -9,12 +9,14 @@ def rr():
     frequency = 125
 
     data = request.get_json()
-    # pass ecg parameter to the function and get the r peaks
-    # ecg model = [-200, -100, -56, 3, 200, ...]
     my_ecg = data['ecg']
 
     out = ecg.ecg(signal=my_ecg, sampling_rate=125, show=False)
+    # pass ecg parameter to the function and get bpms
+    # ecg model = [-200, -100, -56, 3, 200, ...]
     bpm = out["heart_rate"]
+    # pass ecg parameter to the function and get the r peaks
+    # ecg model = [-200, -100, -56, 3, 200, ...]
     rr_peaks = out["rpeaks"]
     int_rr_array = [int(item) for item in rr_peaks]
     int_bpm_array = [int(item) for item in bpm]
