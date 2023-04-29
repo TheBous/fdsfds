6fff9cee4a5b7bc554505ed6dcbb3ca25cbd1e23
Luca Valsecchi <luca.valsecchi@noku.io>
2023-04-29 21:54:36 +0200

    feat(ecg): some comments

--- heartbeat.py
+++ heartbeat.py
@@ -8,10 +8,10 @@ def rr():
     detectors = Detectors(125)
 
     data = request.get_json()
+    # pass ecg parameter to the function and get the r peaks
+    # ecg model = [-200, -100, -56, 3, 200, ...]
     ecg = data['ecg']
-    
     r_peaks_2 = detectors.engzee_detector(ecg)
-
     int_array = [int(item) for item in r_peaks_2]
 
     return jsonify({'rr': int_array})
